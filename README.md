6fcf7380f32118768bc7022bc502f65045a4347f
stevengeorgeharris <mail@stevengeorgeharris.com>
2016-01-14 11:12:41 +0000

    Check date added, stops multiple API calls.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### Astronomy Picture of the Day
 
-A Chrome extension which replaces the default tab with the Astronomy Picture of the Day.
+A Chrome extension which replaces the default tab with the Astronomy Picture of the Day (APOD).
+
+#### Under the hood
+
+We are using the recently released NASA API to retrieve the APOD. 
--- js/modules/app.js
+++ js/modules/app.js
@@ -1,22 +1,60 @@
+/**
+* Astronomy Picture of the Day
+* @author Steven Harris
+*
+*/
+
 var apod = {
   returnedData: {
     // Will replace with API data
   },
+  storeImage: "",
+  sameImage: false,
+  storeDate: localStorage.date,
+  url: "https://api.nasa.gov/planetary/apod?concept_tags=True&api_key=SMSSVoBpeTlTKc8nGXKVcVThKkjHsZSvTx1Xq1Tj",
+  /**
+   * Here we want to record the current date and store it.
+   * Everytime the script is run we check if the date has changed.
+   * This is to stop hundreds of API calls, we should only call it once
+   * per day, as the image only updates once.
+   *
+   */
+  checkDate: function () {
+    var newDate = new Date();
+    var month = newDate.getMonth();
+    var day = newDate.getDay();
+    var checkThis = day + " " + month;
+    if (checkThis === this.storeDate) {
+      this.sameImage = true;
+      this.updateImage();
+    } else {
+      this.call(this.url);
+      localStorage.setItem('date', checkThis);
+    }
+  },
   init: function () {
-    var url = "https://api.nasa.gov/planetary/apod?concept_tags=True&api_key=SMSSVoBpeTlTKc8nGXKVcVThKkjHsZSvTx1Xq1Tj";
-    this.call(url);
+    this.checkDate();
   },
   call: function (url) {
     $.ajax({
       url: url
     }).done(function (data) {
       returnedData = data;
-      console.log(returnedData);
       apod.updateImage();
+      apod.storeLocally();
     });
   },
+  storeLocally: function () {
+    storeImage = returnedData.url; // Use local storage to store URL.
+    localStorage.setItem('image', storeImage);
+  },
   updateImage: function () {
-    var imageURL = returnedData.url;
+    var imageURL;
+    if (this.sameImage === true) {
+      imageURL = localStorage.getItem('image');
+    } else {
+      imageURL = returnedData.url;
+    }
     var $image = $('.apod-image img');
     setTimeout(function() {
         $('.header').addClass('reduce');
